ebc62202630de1c9aca70add8860d161126985a5
IT21810350 Malshan <92059622+IT21810350@users.noreply.github.com>
2022-08-03 03:51:02 +0000

    Have done the 4 exercises.

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,20 @@
 #include <stdio.h>
 
 int main() {
+
+  int subject1, subject2, sum=0;
+  float avg=0;
+
+  printf("Enter marks of first subject: ");
+  scanf("%d", &subject1);
+
+  printf("Enter marks of second subject: ");
+  scanf("%d", &subject2);
+
+  sum = subject1 + subject2;
+  avg = (float)sum/2;
+
+  printf("Average of the two marks is: %.2f", avg);
   
   return 0;
 }
